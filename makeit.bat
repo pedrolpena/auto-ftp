48e0cd5508ce83a7e889dbf02114451d5abd8a6f
pedro.pena <pedro.pena@noaa.gov>
2014-10-25 10:00:25 -0400

    a 1 second sleep was added to rasdial so as to not hog up the CPU

--- makeit.bat
+++ makeit.bat
@@ -1,6 +1,6 @@
 echo off
 
-javac -source 1.6 -target 1.6 -d .\ -cp .\lib\commons-net-3.3.jar;.\lib\sqlitejdbc-v056.jar .\src\autoftp\*.java   
+javac -source 1.7 -target 1.7 -d .\ -cp .\lib\commons-net-3.3.jar;.\lib\sqlitejdbc-v056.jar .\src\autoftp\*.java   
 jar cfm AutoFTP.jar manifest.txt autoftp\*.class
 
 IF EXIST .\dist goto deletedist
--- makeit.sh
+++ makeit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-javac -source 1.6 -target 1.6 -d ./ -cp ./lib/commons-net-3.3.jar:./lib/sqlitejdbc-v056.jar src/autoftp/*.java 
+javac -source 1.7 -target 1.7 -d ./ -cp ./lib/commons-net-3.3.jar:./lib/sqlitejdbc-v056.jar src/autoftp/*.java 
 jar cfm AutoFTP.jar manifest.txt autoftp/*.class 
 if [ -d "dist" ]; then
     rm -r dist
